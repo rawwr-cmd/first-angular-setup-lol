95d32f42a7c9df89a23fac014374b0c63c0629d9
rawwr <akshayace0302@gmail.com>
2023-02-03 21:17:02 +0530

    initialization2

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+//JIT -- just in time compilation
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
