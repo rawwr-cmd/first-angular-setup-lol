2bebc9ebfdc0ec763ebe7ec0e39396f2862df35f
rawwr <akshayace0302@gmail.com>
2023-02-03 23:11:16 +0530

    creating component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<p>My First Angular 2 App</p>`,
 })
-export class AppComponent {
-  title = 'basics';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+//module is exported as class in angular
+import { NgModule, enableProdMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 
+enableProdMode();
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  // Declarations in Angular refer to the process of registering a
+  // component, directive, pipe, or service with the Angular application.
+  declarations: [AppComponent],
+  // list of components for bootstraping other
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+// In Angular, "bootstrapping" refers to the process of
+// launching or starting an Angular application. When an Angular
+// application starts, the root module (also known as the "bootstrap module")
+// is automatically loaded, and its
+// components are used to render the application in the browser.
